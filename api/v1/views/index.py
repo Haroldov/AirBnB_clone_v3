d76a2a3da5c3cea62db3dc0427f143da7fb6a4f7
root <hvelez020@gmail.com>
2019-09-12 02:27:43 +0000

    [fix] pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,16 +6,21 @@ from api.v1.views import app_views
 from flask import jsonify
 import models
 
+
 @app_views.route("/status")
 def status():
     """Returns the status OK"""
     return jsonify({"status": "OK"})
 
+
 @app_views.route("/stats")
 def stats():
     """Returns the stats"""
     classes = {"Amenity": "amenities", "City": "cities",
-               "User": "users", "Place": "places", "Review": "reviews",
+               "User": "users", "Place": "places",
+               "Review": "reviews",
                "State": "states"}
-    new_dict = map(lambda tpl: (tpl[1], models.storage.count(tpl[0])), classes.items())
+    new_dict = map(lambda tpl: (tpl[1],
+                                models.storage.count(tpl[0])),
+                   classes.items())
     return jsonify(dict(new_dict))
