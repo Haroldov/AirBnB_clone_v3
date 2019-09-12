4eec1520047b88eae3f039de328b6fa2cd615bca
root <hvelez020@gmail.com>
2019-09-12 02:14:23 +0000

    [add] task #5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,16 @@ Module for index
 """
 from api.v1.views import app_views
 from flask import jsonify
-
+import models
 
 @app_views.route("/status")
 def status():
     """Returns the status OK"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def stats():
+    """Returns the stats"""
+    classes = ["Amenity", "City", "User", "Place", "Review", "State"]
+    new_dict = map(lambda name: (name, models.storage.count(name)), classes)
+    return jsonify(dict(new_dict))
